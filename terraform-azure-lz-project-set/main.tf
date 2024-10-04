8c31634cb12b292e96c4fdf302e0c69f5f730747
Warren Uniewski <warren@autimo.com>
2024-10-04 16:28:41 -0700

    fix for_each

--- terraform-azure-lz-project-set/main.tf
+++ terraform-azure-lz-project-set/main.tf
@@ -66,10 +66,7 @@ module "lz_vending" {
 
 # Create budgets directly using azurerm provider instead of the lz-vending module
 resource "azurerm_consumption_budget_subscription" "subscription_budget" {
-  for_each = {
-    for k, v in var.subscriptions : k => v
-    if v.budget_amount > 0
-  }
+  for_each = var.subscriptions
 
   name            = "budget-${var.license_plate}-${each.value.name}"
   subscription_id = module.lz_vending[each.key].subscription_id
@@ -82,7 +79,7 @@ resource "azurerm_consumption_budget_subscription" "subscription_budget" {
   }
 
   notification {
-    enabled        = true
+    enabled        = each.value.budget_amount > 0
     threshold      = 80.0
     operator       = "GreaterThanOrEqualTo"
     threshold_type = "Actual"
@@ -91,7 +88,7 @@ resource "azurerm_consumption_budget_subscription" "subscription_budget" {
   }
 
   notification {
-    enabled        = true
+    enabled        = each.value.budget_amount > 0
     threshold      = 100.0
     operator       = "GreaterThan"
     threshold_type = "Forecasted"
