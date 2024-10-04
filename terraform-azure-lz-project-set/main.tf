923267d61ec0142ba9d849fce8e62651bdb1ee77
Warren Uniewski <warren@autimo.com>
2024-10-04 16:32:43 -0700

    use the subscription_resource_id

--- terraform-azure-lz-project-set/main.tf
+++ terraform-azure-lz-project-set/main.tf
@@ -69,7 +69,7 @@ resource "azurerm_consumption_budget_subscription" "subscription_budget" {
   for_each = var.subscriptions
 
   name            = "budget-${var.license_plate}-${each.value.name}"
-  subscription_id = module.lz_vending[each.key].subscription_id
+  subscription_id = module.lz_vending[each.key].subscription_resource_id
 
   amount     = each.value.budget_amount
   time_grain = "Monthly"
