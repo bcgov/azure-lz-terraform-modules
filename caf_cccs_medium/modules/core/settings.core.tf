611d46d2fbef76352821fa3015453a24b0cff36e
Adin Ermie <Adin.Ermie@outlook.com>
2025-01-21 16:16:16 -0500

    Removed false DNS value

--- caf_cccs_medium/modules/core/settings.core.tf
+++ caf_cccs_medium/modules/core/settings.core.tf
@@ -30,7 +30,7 @@ locals {
           scopeToSupportedImages = true
         },
         Deny-VNet-DNS-Changes = {
-          VNet-DNS-Settings = var.root_id == "bcgov-managed-lz-forge" ? ["10.41.253.4", "10.41.253.5"] : ["10.53.244.4"]
+          VNet-DNS-Settings = var.root_id == "bcgov-managed-lz-forge" ? ["10.41.253.4"] : ["10.53.244.4"]
         }
       }
       access_control = {}
