510589123809e6d13c66a7b050ee9e2b554555db
Adin Ermie <Adin.Ermie@outlook.com>
2025-04-01 14:54:42 -0400

    Code cleanup, comments, and added CODEOWNERS

--- azure_private_dns/private_dns_resolver/locals.tf
+++ azure_private_dns/private_dns_resolver/locals.tf
@@ -2,7 +2,6 @@ locals {
   subnets = {
     for subnet in var.virtual_network_object.subnet : subnet.name => merge(
       subnet, {
-        # first_available_ip = cidrhost(subnet.address_prefixes, 4)
         first_available_ip = cidrhost(element(subnet.address_prefixes, 0), 4)
         # This extracts the fourth IP address from the subnet address prefix, which is the first available IP address in the subnet (as Azure reserves the first three IP addresses in each subnet)
       }
