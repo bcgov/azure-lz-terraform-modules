118e85533400ddcb029ec0f8881d4b826a2641aa
Adin Ermie <Adin.Ermie@outlook.com>
2025-09-09 15:37:32 -0400

    Removed locals ref for validation

--- azure_ip_group/locals.tf
+++ azure_ip_group/locals.tf
@@ -1,6 +1,3 @@
 locals {
   subscription_id_connectivity = coalesce(var.subscription_id_connectivity, data.azurerm_client_config.current.subscription_id)
-
-  # IP range validation regex pattern (extracted for readability)
-  ip_range_regex = "^((25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\-((25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)$"
 }
--- azure_ip_group/variables.ip_group.tf
+++ azure_ip_group/variables.ip_group.tf
@@ -20,7 +20,7 @@ variable "ip_group" {
         for ip in obj.ip_addresses : (
           provider::assert::cidrv4(ip) ||
           provider::assert::ipv4(ip) ||
-          can(regex(local.ip_range_regex, ip))
+          can(regex("^((25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\-((25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)$", ip))
         )
       ])
     ])
