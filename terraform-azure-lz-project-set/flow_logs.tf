8e9831e08b9d34f0444c67751b2aeeea41d271ec
Harsimran Singh Maan <maan.harry@gmail.com>
2025-11-18 16:22:20 -0800

    Add v2

--- terraform-azure-lz-project-set/flow_logs.tf
+++ terraform-azure-lz-project-set/flow_logs.tf
@@ -1,13 +1,13 @@
 resource "azurerm_network_watcher_flow_log" "flowlogs" {
   for_each             = { for sub in var.subscriptions : sub.name => sub if try(sub.network.enabled, false) }
-  network_watcher_name = format("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/networkWatchers/NetworkWatcher_%s", module.lz_vending[each.key].subscription_resource_id, local.NetworkWatcherRGName, lower(var.primary_location))
+  network_watcher_name = format("NetworkWatcher_%s", lower(var.primary_location))
   resource_group_name  = local.NetworkWatcherRGName
   name                 = "${var.license_plate}-${each.value.name}-vwan-spoke-${var.license_plate}-${each.value.name}-networking-flowlog"
 
   target_resource_id = module.lz_vending[each.value.name].virtual_network_resource_ids["vwan_spoke"]
   storage_account_id = var.vnet_flow_logs_storage_account_id
   enabled            = true
-
+  version            = 2
   retention_policy {
     enabled = true
     days    = 3
