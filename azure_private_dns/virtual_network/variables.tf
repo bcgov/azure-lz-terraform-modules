366800ce0a9d1a6ccc717909fcf05155e0c0980e
Harsimran Singh Maan <maan.harry@gmail.com>
2025-12-17 10:08:17 -0800

    remove ipam provider

--- azure_private_dns/virtual_network/variables.tf
+++ azure_private_dns/virtual_network/variables.tf
@@ -1,11 +1,3 @@
-variable "IPAM_TOKEN" {
-  description = "(Required) The IPAM token to use for IP address management."
-  type        = string
-  sensitive   = true
-  # az account get-access-token --resource api://8b672441-25f3-4a33-8336-d853c466a782
-  # For local testing, add the IPAM token to an environment variable using: export TF_VAR_IPAM_TOKEN="<ACCESS_TOKEN_VALUE>"
-}
-
 variable "environment" {
   description = "(Required) This is either LIVE or FORGE."
   type        = string
