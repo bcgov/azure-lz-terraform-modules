ef5ff15b471df2cb4169abbb99085df63f097a2b
Adin Ermie <Adin.Ermie@outlook.com>
2026-03-30 17:17:45 -0400

    Initial AKS CIDR policy

--- caf_cccs_medium/modules/core/settings.core.tf
+++ caf_cccs_medium/modules/core/settings.core.tf
@@ -66,6 +66,12 @@ locals {
         Deny-VNet-DNS-Changes = {
           VNet-DNS-Settings = var.VNet-DNS-Settings
         },
+        Enforce-AKS-CIDRs = {
+          allowedPodCidrRanges     = ["10.10.0.0/16"],
+          enforceServiceCidr       = false,
+          allowedServiceCidrRanges = ["10.20.0.0/16"],
+          effect                   = "Deny",
+        },
         Deny-Delete-NetworkWatch = {
           Network-Watcher-storageId           = "/subscriptions/${var.subscription_id_management}/resourceGroups/${var.network_watcher_storage_account_resource_group}/providers/Microsoft.Storage/storageAccounts/${var.network_watcher_storage_account_name}"
           Network-Watcher-workspaceResourceId = "/subscriptions/${var.subscription_id_management}/resourceGroups/${var.root_id}-mgmt/providers/Microsoft.OperationalInsights/workspaces/${var.root_id}-la"
